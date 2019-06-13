29d978b088a1be9bb6d8f26ca22e2a723f8e3b7d
olabudzyn <ola.budz96@gmail.com>
2019-06-13 02:38:47 +0200

    delete ward works

--- src/main/java/com/isieiti/bdproject/entity/Ward.java
+++ src/main/java/com/isieiti/bdproject/entity/Ward.java
@@ -2,20 +2,25 @@ package com.isieiti.bdproject.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
+import java.util.List;
+
+import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Data
 @Table(name = "wards")
 public class Ward {
+
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     private String name;
+
+    @OneToMany(cascade = ALL, orphanRemoval = true)
+    @JoinColumn(name = "ward_id")
+    private List<Employee> employees;
 }
